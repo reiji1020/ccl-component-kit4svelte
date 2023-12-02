5d250bf62ce768fd229e3ace0ef47616c904bf99
reiji1020 <sk@reiji1020.info>
2023-12-02 14:06:45 +0900

    [add]add storybook

---
+++ dist/Header.svelte.d.ts
@@ -0,0 +1,23 @@
+/** @typedef {typeof __propDef.props}  HeaderProps */
+/** @typedef {typeof __propDef.events}  HeaderEvents */
+/** @typedef {typeof __propDef.slots}  HeaderSlots */
+export default class Header extends SvelteComponentTyped<{
+    [x: string]: never;
+}, {
+    [evt: string]: CustomEvent<any>;
+}, {}> {
+}
+export type HeaderProps = typeof __propDef.props;
+export type HeaderEvents = typeof __propDef.events;
+export type HeaderSlots = typeof __propDef.slots;
+import { SvelteComponentTyped } from "svelte";
+declare const __propDef: {
+    props: {
+        [x: string]: never;
+    };
+    events: {
+        [evt: string]: CustomEvent<any>;
+    };
+    slots: {};
+};
+export {};
