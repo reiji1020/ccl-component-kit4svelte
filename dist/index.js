be6832e29fab73d3e6c10a29d49c1c1a1fc7f466
reiji1020 <sk@reiji1020.info>
2024-02-06 02:36:50 +0900

    [update]カスタムプロパティ読み込み部分の修正

--- dist/index.js
+++ dist/index.js
@@ -1,4 +1,4 @@
 // place files you want to import through the `$lib` alias in this folder.
-import '$lib/const/variables.css'; // カスタムプロパティ読み込み用
+import './const/variables.css'; // カスタムプロパティ読み込み用
 export { default as Header } from './Header.svelte';
 export { default as Button } from './Button.svelte';
