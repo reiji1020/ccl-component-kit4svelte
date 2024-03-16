dafea5b441843e099883999f33760d28cbd7a974
reiji1020 <sk@reiji1020.info>
2024-03-16 20:18:44 +0900

    [update]パッケージアップデートとdistファイルの削除

--- dist/index.d.ts
+++
@@ -1,6 +0,0 @@
-import './const/variables.css';
-export { default as Header } from './Header.svelte';
-export { default as Button } from './Button.svelte';
-export { default as Footer } from './Footer.svelte';
-export { default as Thumbnail } from './Thumbnail.svelte';
-export { default as Card } from './Card.svelte';
--- dist/index.js
+++
@@ -1,7 +0,0 @@
-// place files you want to import through the `$lib` alias in this folder.
-import './const/variables.css'; // カスタムプロパティ読み込み用
-export { default as Header } from './Header.svelte';
-export { default as Button } from './Button.svelte';
-export { default as Footer } from './Footer.svelte';
-export { default as Thumbnail } from './Thumbnail.svelte';
-export { default as Card } from './Card.svelte';
