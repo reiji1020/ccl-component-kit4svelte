d33d491fe1ecf3f112886936a73dc7ba3a8c4274
reiji1020 <sk@reiji1020.info>
2024-03-16 10:22:13 +0900

    [update]Storybook8対応

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -3,11 +3,10 @@ import type { StorybookConfig } from '@storybook/sveltekit';
 const config: StorybookConfig = {
 	stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
 	addons: [
-		'@storybook/addon-links',
-		'@storybook/addon-essentials',
-		'@storybook/addon-interactions',
-		'@storybook/addon-actions'
-	],
+        '@storybook/addon-links',
+        '@storybook/addon-essentials',
+        '@storybook/addon-interactions',
+    ],
 	framework: {
 		name: '@storybook/sveltekit',
 		options: {}
